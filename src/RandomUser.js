d2f07c7993dda527b6eaaade6c115ef1dfbde257
farizmursyidan <farizmursyidan@gmail.com>
2019-08-26 17:03:15 +0700

    added search, sort, and loading indicator features

--- src/RandomUser.js
+++ src/RandomUser.js
@@ -1,88 +1,148 @@
 import React from 'react';
+import { trackPromise } from 'react-promise-tracker';
 
 export class RandomUser extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			items: [],
+			items: []
+		};
+	}
+
+	componentDidMount() {
+		trackPromise(
+			fetch("https://randomuser.me/api/?results=12")
+				.then(res => res.json())
+				.then(parsedJSON => parsedJSON.results.map(data => (
+					{
+						name: `${data.name.first} ${data.name.last}`,
+			            location: `${data.location.state}, ${data.nat}`,
+			            thumbnail: `${data.picture.large}`,
+			            email: `${data.email}`,
+			            phone: `${data.phone}`,
+			            username: `${data.login.username}`
+					}	
+				)))
+				.then(items => this.setState({
+					items, isLoaded: false
+				}))
+				.catch(error => console.log('parsing failed', error))
+		);
+	}
+
+	render() {
+		return (
+			<div>
+				<Search items={this.state.items} />
+			</div>
+		);
+	}
+}
+
+class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
 			filtered: []
 		};
 		this.handleChange = this.handleChange.bind(this);
 	}
 
 	componentDidMount() {
-		fetch("https://randomuser.me/api/?results=12")
-			.then(res => res.json())
-			.then(parsedJSON => parsedJSON.results.map(data => (
-				{
-					firstName: `${data.name.first}`,
-		            lastName: `${data.name.last}`,
-		            location: `${data.location.state}, ${data.nat}`,
-		            thumbnail: `${data.picture.large}`,
-		            email: `${data.email}`,
-		            phone: `${data.phone}`,
-		            username: `${data.login.username}`
-				}	
-			)))
-			.then(items => this.setState({
-				items, isLoaded: false
-			}))
-			.catch(error => console.log('parsing failed', error));
-
 		this.setState({
 			filtered: this.props.items
 		})
 	}
 
-	componentWillReceiveProps(nextProps) {
+	UNSAFE_componentWillReceiveProps(nextProps) {
 		this.setState({
 			filtered: nextProps.items
 		})
 	}
 
+	handleChange(e) {
+		let currentList = [];
+		let newList = [];
+
+		if(e.target.value !== "") {
+			currentList = this.props.items;
+
+			newList = currentList.filter(item => {
+				const lc = item.name.toLowerCase();
+				const filter = e.target.value.toLowerCase();
+				return lc.includes(filter);
+			});
+		}
+		else {
+			newList = this.props.items;
+		}
+
+		this.setState({
+			filtered: newList
+		});
+	}
+
 	render() {
-		const {items} = this.state;
+		const {filtered} = this.state;
+    	function compare(a,b) {
+      		const nameA = a.name;
+      		const nameB = b.name;
+
+      		let comparison = 0;
+		    if (nameA > nameB) {
+		    	comparison = 1;
+		    } else if (nameA < nameB) {
+		    	comparison = -1;
+		    }
+      		return comparison;
+    	}
+
+    	const fungsisort = () => {
+      		console.log(filtered.sort(compare));
+      		this.setState({ filtered: filtered })
+    	}
 
 		return (
 			<div>
 				<div className="row">
-                    <div className="col-md-12">                        
-                        <div className="panel panel-default">
-                            <div className="panel-body">
-                                <p>Use search to find contacts. You can search by: name, address, phone. Or use the advanced search.</p>
-                                <form className="form-horizontal">
-                                    <div className="form-group">
-                                        <div className="col-md-8">
-                                            <div className="input-group">
-                                                <div className="input-group-addon">
-                                                    <span className="fa fa-search"></span>
-                                                </div>
-                                                <input type="text" className="form-control" onChange={this.handleChange} placeholder="Who are you looking for?"/>
-                                                <div className="input-group-btn">
-                                                    <button className="btn btn-primary">Search</button>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>                                    
-                            </div>
-                        </div>                        
-                    </div>
-                </div>
-                <div className="row">
+	                <div className="col-md-12">                        
+	                    <div className="panel panel-default">
+	                        <div className="panel-body">
+	                            <p>Use search to find contacts. You can search by name</p>
+	                            <form className="form-horizontal">
+	                                <div className="form-group">
+	                                    <div className="col-md-8">
+	                                        <div className="input-group">
+	                                            <div className="input-group-addon">
+	                                                <span className="fa fa-search"></span>
+	                                            </div>
+	                                            <input type="text" className="form-control" onChange={this.handleChange} placeholder="Who are you looking for?"/>
+	                                        </div>
+	                                    </div>
+	                                </div>
+	                            </form>     
+	                            <br />                   
+	                            <button className="btn btn-primary" onClick={fungsisort}>
+	                  				Sort By Name
+	                			</button>            
+	                        </div>
+	                    </div>                        
+	                </div>
+	            </div>
+	            <div className="row">
 					{
-						items.length > 0 ? items.map(item => {
-							const {firstName, lastName, location, thumbnail, email, phone, username} = item;
+						this.state.filtered.map(item => {
+							const {name, location, thumbnail, email, phone, username} = item;
 							return (
 								<div key={phone}>
 									<div className="col-md-3">
 			                            <div className="panel panel-default">
 			                                <div className="panel-body profile">
 			                                    <div className="profile-image">
-			                                        <img src={thumbnail} alt={firstName} />
+			                                        <img src={thumbnail} alt={name} />
 			                                    </div>
 			                                    <div className="profile-data">
-			                                        <div className="profile-data-name">{firstName} {lastName}</div>
+			                                        <div className="profile-data-name">{name}</div>
 			                                        <div className="profile-data-title">{username}</div>
 			                                    </div>
 			                                    <div className="profile-controls">
@@ -100,33 +160,11 @@ export class RandomUser extends React.Component {
 			                            </div>
 			                        </div>
 		                        </div>
-							);
-						}) : null
-					};
+							)
+						})
+					}
 				</div>
 			</div>
 		);
-	}
-
-	handleChange(e) {
-		let currentList = [];
-		let newList = [];
-
-		if(e.target.value !== "") {
-			currentList = this.props.items;
-
-			newList = currentList.filter(item => {
-				const lc = item.toLowerCase();
-				const filter = e.target.value.toLowerCase();
-				return lc.includes(filter);
-			});
-		}
-		else {
-			newList = this.props.items;
-		}
-
-		this.setState({
-			filtered: newList
-		});
 	}
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {RandomUser} from './RandomUser';
 import * as serviceWorker from './serviceWorker';
+import { usePromiseTracker } from "react-promise-tracker";
 
-ReactDOM.render(<RandomUser />, document.getElementById('root'));
+const LoadingIndicator = props => {
+	const { promiseInProgress } = usePromiseTracker();
+	return (
+		promiseInProgress && 
+		<h1><i className="fa fa-spinner fa-spin" /> Loading...</h1>
+ 	);  
+}
+
+ReactDOM.render(<div><RandomUser /><LoadingIndicator/></div>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
